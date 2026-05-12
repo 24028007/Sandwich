dabce545b879745532b0d68ec470b3e0779f6309
ROLIVHUWA NEFEFE <24000975>
2026-05-12 14:02:57 +0200

    First commit

---
+++ src/main/java/com/mycompany/jsandwich/JSandwich.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.jsandwich;
+
+/**
+ *
+ * @author nefef
+ */
+public class JSandwich extends javax.swing.JFrame {
+
+    public static void main(String[] args) {
+        //System.out.println("Hello World!");
+        java.awt.EventQueue.invokeLater(()->{new JSandwich().setVisible(true);
+        });
+    }
+}
